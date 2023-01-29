84d83f9261e3f6ca40c17e9f89dcdb687a0b0cc4
AnastasiaRyseva <92371518+AnastasiaRyseva@users.noreply.github.com>
2023-01-29 21:50:31 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
 //функция для смены класса у блока
 const changeActiveBlock = function(block, nameClass) {
-    block.classList.toggle(`${nameClass}`)
+    block.classList.toggle(nameClass)
 }
-//анимация бургер-меню
+//анимация и поведение мобильного меню
 const headerBurger = document.querySelector('.header-burger');
+const menu = document.querySelector('.header-nav');
+const menuLink = document.querySelectorAll('.header-link');
+
 headerBurger.addEventListener('click', () => {
   changeActiveBlock(headerBurger, 'active');
+  changeActiveBlock(menu, 'mobile');
 })
+menuLink.forEach(link => link.addEventListener('click', () => {
+  changeActiveBlock(menu, 'mobile');
+  changeActiveBlock(headerBurger, 'active');
+}))
 
 //слайдер
 new Swiper('.mySwiper', {
